63b125d0a6dbcc7973b9f828ac81db5bf9c62089
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2015-04-17 09:39:40 +0530

    Abhinav/Vidhi - View Pager Implemented.

--- app/src/main/java/bootcamp/android/activities/ProductDetailsActivity.java
+++ app/src/main/java/bootcamp/android/activities/ProductDetailsActivity.java
@@ -3,14 +3,19 @@ package bootcamp.android.activities;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
 import androidplugins.Callback;
 import androidplugins.imagefetcher.ImageFetcher;
 import bootcamp.android.R;
+import bootcamp.android.adapters.ProductDetailsPagerAdapter;
 import bootcamp.android.fragments.ProductDetailsFragment;
 import bootcamp.android.models.Product;
 
@@ -25,9 +30,11 @@ public class ProductDetailsActivity extends FragmentActivity {
     setContentView(R.layout.product_details_container);
 
     Bundle extraArguments = getIntent().getExtras();
-    ProductDetailsFragment productDetailsFragment = new ProductDetailsFragment();
-    productDetailsFragment.setArguments(extraArguments);
-    getSupportFragmentManager().beginTransaction().add(R.id.product_details_container, productDetailsFragment, "products_fragment").commit();
+    ArrayList<Product> products = extraArguments.getParcelableArrayList(PRODUCTS_KEY);
+    int current = extraArguments.getInt(CURRENT_PRODUCT_KEY);
+    ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+    viewPager.setAdapter(new ProductDetailsPagerAdapter(getSupportFragmentManager(),products));
+    viewPager.setCurrentItem(current);
   }
 
 
